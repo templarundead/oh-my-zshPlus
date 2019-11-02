f18d992067a32d4dee1cb7dca145e818b3e1dc34
TemplarUndead <templarundead@users.noreply.github.com>
2019-11-02 21:23:27 +0300

    Added "scripts" plugin

---
+++ plugins/scripts/scripts-aliases.plugin.zsh
@@ -0,0 +1,9 @@
+# Shell scripts from '.scripts' folder
+alias bro='browserify.sh'
+alias csso='csso.sh'
+alias moz='mozjpeg.sh'
+alias opng='oxipng.sh'
+alias svgo='svgo.sh'
+
+# System/Hardware info
+alias temp='grep "[0-9]" /sys/class/thermal/thermal_zone0/temp'
